47df6ab6e2583240b62c6cc12af5ad7a47ced4e9
danielam23 <85262383+danielam23@users.noreply.github.com>
2021-06-02 18:17:26 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+// import PrimeraApp from './PrimeraApp';
+import CounterApp from './CounterApp'
+
+import './index.css'
+
+const divRoot = document.querySelector('#root');
+
+ReactDOM.render(<CounterApp value={ 5 }/>, divRoot);
